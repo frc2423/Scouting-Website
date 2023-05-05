07a6580dc74536c59d1b54a01ff4c66a1af7be66
Jacob Heller <jacobgheller@gmail.com>
2023-05-05 08:42:34 -0400

    added initial team list view

---
+++ src/components/TeamRow.vue
@@ -0,0 +1,27 @@
+<script>
+    export default{
+        data(){
+            return {
+                allCapabilities: ["score_high", "score_low", "cones", "cubes", "auto"]
+            }
+        },
+        props: ['teamName', 'location']
+    }
+</script>
+<template>
+    <h2>{{ teamName }}</h2>
+    <div>{{ location }}</div>
+    <div class="incapable" v-for="capability in allCapabilities">
+        {{ capability }}
+    </div>
+</template>
+
+<style scoped>
+    .capable{
+        color: green
+    }
+
+    .incapable{
+        color: red
+    }
+</style>
---
+++ src/views/TeamListView.vue
@@ -0,0 +1,33 @@
+<script>
+import TeamRow from '../components/TeamRow.vue'
+
+export default{
+    components: {
+        TeamRow
+    },
+    data(){
+        return{
+            teams:[
+                {
+                    name: "The Kwarqs", 
+                    location: "Watertown, MA"},
+                    capabilities: ["score_high", "score_low", "cones", "cubes", "auto"]
+                {
+                    name: "The Highlanders", 
+                    location: "Somerville, MA",
+                    capabilities: ["score_high", "score_low"]
+                }
+            ],
+        }
+    }
+}
+</script>
+
+
+<template>
+    <div>
+        <h1>Team List</h1>
+        <TeamRow v-for="team in teams" :key="team.name" :teamName="team.name" :location="team.location" />
+
+    </div>
+</template>
